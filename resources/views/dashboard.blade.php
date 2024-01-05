2b95604a30963b5ef22b79d5986cfa41305cdabd
Khalishi <khalishisimon@gmail.com>
2024-01-05 15:50:53 +0200

    implementation ui

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,18 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="h2 font-weight-bold text-dark">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+    <div class="py-3">
+        <div class="container mx-auto px-4">
+            <div class="card shadow-sm">
+                <div class="card-body text-dark">
+                    <livewire:client.client-index />
                 </div>
             </div>
         </div>
     </div>
 </x-app-layout>
+
